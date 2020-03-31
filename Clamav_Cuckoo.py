5409b113884556b8c66a0188ab754dc924896603
LongLengoc <longln784923@gmail.com>
2020-04-01 02:39:10 +0700

    Update1
    Using Clamd, Cuckoo API

--- Clamav_Cuckoo.py
+++ Clamav_Cuckoo.py
@@ -8,75 +8,65 @@ Linux, MacOSX and other platforms.
 """
 
 class build():
-    @staticmethod
-    def run_clamd():
+    def __init__(self):
+        # File location
+        self.file = "/home/cuckoo/Documents/test"
 
-        # test if server is reachable
+    def run_clamd(self):
+        # Test if server is reachable
         cd = clamd.ClamdUnixSocket()
-        # file or directory you want to check
-        file1 = ""
-        # scan
-        cd.scan(file1)
+        # Scan
+        cd.scan(self.file)
 
         """
         output cd.scan(file)
         - if ok
             > {u'file': (u'OK', None)}
         - if detect something
-            > {'/tmp/EICAR': ('FOUND', 'Eicar-Test-Signature')}             
+            > {u'/tmp/EICAR': (u'FOUND', 'Eicar-Test-Signature')}             
         """
 
-        result = cd.scan(file1)
-        if (result['file1'][0] == "FOUND"):
+        result = cd.scan(self.file)
+        print(result)
+        #if (result[self.file][0] == "OK"):
             # direct to cuckoo
-            run_cuckoo(file1)
-        else:
+            #run_cuckoo(self)
+        #else:
             # file clean
-            return 
+            #return 
 
-    @staticmethod
-    def run_cuckoo(file1):
+    def run_cuckoo(self):
         # By default it will bind the service on localhost:8090
 
         # POST /tasks/create/file: Adds a file to the list of pending tasks to be processed and analyzed.
         # Adds a file to the list of pending tasks. Returns the ID of the newly created task
 
         """
-        To allow only authenticated access to the API, the api_token in cuckoo.conf must be set to a secret value. 
-        In new Cuckoo installations, a random token is automatically generated for you. 
-        To access the API, you must send the Authorization: Bearer <token> header with all your requests using the token defined in the configuration.
-        Note that if you want to access the API over an insecure network such as the Internet, 
-        you should run the API server behind nginx described in the next section and enable HTTPS.
-        """
-        
-        """
+        Location : $CWD/conf/cuckoo.conf
         # The authentication token that is required to access the Cuckoo API, using
         # HTTP Bearer authentication. This will protect the API instance against
         # unauthorized access and CSRF attacks. It is strongly recommended to set this
         # to a secure value.
-        api_token = TeYLAFvaeYVr01dBQ4VybQ
+        api_token = DGC3sBrPgVvOud3voKCong
         """
 
-        REST_URL = "http://localhost:8000/tasks/create/file"
-        #REST_URL = "http://localhost:8000/submit/api/filetree/"
-        #REST_URL1 = "http://localhost:8000/submit/api/submit"
-        SAMPLE_FILE = file1
-        HEADERS = {"Authorization" : "Bearer TeYLAFvaeYVr01dBQ4VybQ"}
+        REST_URL = "http://localhost:8090/tasks/create/file"
+        SAMPLE_FILE = self.file
+        HEADERS = {"Authorization" : "Bearer DGC3sBrPgVvOud3voKCong"}
 
         with open(SAMPLE_FILE, "rb") as sample:
             files = {"file": ("temp_file_name", sample)}
             r = requests.post(REST_URL, headers=HEADERS, files=files)
-            print (r.status_code) 
-            #r1 = requests.post(REST_URL1, headers=HEADERS, files=files)
-            #print (r1.status_code)
+            print ("Status code : " + str(r.status_code))
 
         # check task id
-        #task_id = r.json()["task_id"]
-        #print(task_id)
+        task_id = r.json()["task_id"]
+        print(task_id)
 
 def main():
     buildproject = build()
     buildproject.run_clamd()
+    buildproject.run_cuckoo()
 
 if __name__ == '__main__':
     main()
